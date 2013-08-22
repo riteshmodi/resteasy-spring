c45fe5bb96ce4a1366c122dc8cdf1910adb719ed
riteshmodi <ritesh.ked3@gmail.com>
2013-08-22 15:07:21 +0530

    Integrating resource file with DAO

--- src/main/java/org/resteasy/rest/SampleResource.java
+++ src/main/java/org/resteasy/rest/SampleResource.java
@@ -5,26 +5,35 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.resteasy.SampleDAO;
 import org.resteasy.model.Person;
 
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
-@Produces
+//@Produces
 @Path("/person")
 @Api(value="/person",description="person api")
 public class SampleResource {
+	
+	private SampleDAO sampleDAO;
 	
 	@GET
 	@Path("/all")
 	@Produces(MediaType.APPLICATION_JSON)
 	@ApiOperation(value="message print",response=Person.class)
-	public Person printPerson() {
-		Person person1 = new Person();
+	public List<Person> printPerson() {
+		/*Person person1 = new Person();
 		person1.setName("Ramesh");
 		person1.setAge(26);
 		//return context.getBean(Person.class);
-		return person1;
+		return person1;*/
+		
+		return sampleDAO.getAllPersons();
+	}
+	
+	public void setSampleDAO(SampleDAO sampleDAO) {
+		this.sampleDAO = sampleDAO;
 	}
 
 }
