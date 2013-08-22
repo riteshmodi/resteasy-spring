ba327285862d9e0e93752072639a89a7d00cec46
riteshmodi <ritesh.ked3@gmail.com>
2013-08-22 11:42:48 +0530

    corrected comile time errors

--- src/main/java/org/resteasy/MyCustomBootstrap.java
+++ src/main/java/org/resteasy/MyCustomBootstrap.java
@@ -1,12 +1,12 @@
 package org.resteasy;
 
-import com.wordnik.swagger.jaxrs.*;
 import com.wordnik.swagger.config.*;
-import com.wordnik.swagger.model.*;
 
 import javax.servlet.http.HttpServlet;
 
 public class MyCustomBootstrap extends HttpServlet {
+  
+  private static final long serialVersionUID = 100L;
   static {
     ConfigFactory.config().setBasePath("http://localhost:8080/spring");
   }
