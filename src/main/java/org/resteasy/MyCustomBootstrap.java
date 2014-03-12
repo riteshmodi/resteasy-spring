06114559bec7ba936acf46e98a271a3fe55e3c72
riteshmodi <ritesh.ked3@gmail.com>
2014-03-12 13:19:59 +0530

    Update MyCustomBootstrap.java

--- src/main/java/org/resteasy/MyCustomBootstrap.java
+++ src/main/java/org/resteasy/MyCustomBootstrap.java
@@ -9,5 +9,12 @@ public class MyCustomBootstrap extends HttpServlet {
   private static final long serialVersionUID = 100L;
   static {
     ConfigFactory.config().setBasePath("http://localhost:8080/spring");
+   
+   //use the following as another option
+    /*BeanConfig bean = new BeanConfig();
+		bean.setScan(true);
+		bean.setResourcePackage("org.resteasy.rest");
+		bean.setBasePath("http://localhost:8080/spring");
+		bean.setVersion("1.0");*/
   }
 }
